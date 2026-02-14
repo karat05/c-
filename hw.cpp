ed9a97ee523bdcd6a94c22e8e1ef17f4864001ac
karat05 <yaric23x@gmail.com>
2026-02-14 12:35:48 +0000

    gg

--- hw.cpp
+++ hw.cpp
@@ -1,21 +1,56 @@
 #include <iostream>
-// int main (){
-//  //int a;
-//    // std::cin >> a;
-//     std::cout <<"Hello, World"<<std::endl;
-//     std::cerr <<"ERROR!"<<std::endl;
-//  return a;
+#include <string>
+
+
+void getHelp()
+{
+std::cout <<"Help pls" << ;
+}
  int main(int argc, char*argv[]){
-     int nail[9];
-     for (int i=0;i<9;i++){
-        nail[i]=i;
-    
-    }
-    for(int i=0;i<9;i++){
-        std::cout<<"nail[" << i << "]="<<nail[i]<<"\n";
+    std::string helpstring;
+    bool neehelp = false;
+    helpstring = "--help";
+
+
+    for (int i=0; i < argc; i++){
+        std::cout << "argv [" << i <<"] = " <<argv[ i ] << "\n";
+        if(helpstring == argv[i]) {
+            needhelp = true;
+        }
+
+
 
     }
- }
-    for (int i=0;i<argc; i++){
-        std::cout<< "argv[" << i << "]" <<argv[i] << "\n";
+    if (needhelp){
+
     }
+ 
+ //int main(int argc, char*argv[]){
+ //    int nail[9];
+  //   for (int i=0;i<9;i++){
+  //      nail[i]=i;
+    
+  ////  }
+  //  for(int i=0;i<9;i++){
+  //      std::cout<<"nail[" << i << "]="<<nail[i]<<"\n";
+
+ //   }
+// }
+//    for (int i=0;i<argc; i++){
+ //       std::cout<< "argv[" << i << "]" <<argv[i] << "\n";
+ //   }
+
+ //int main (){
+// int a;
+ //    std::cin >> a;
+ //    std::cout <<"Hello, World"<<std::endl;
+ //    std::cerr <<"ERROR!"<<std::endl;
+  return 0;
+ }
+ //std::string string;
+ //   std::cin >> string;
+ //   if(string=="--help"){
+ //       getHelp();
+ //   };
+ //
+ //
